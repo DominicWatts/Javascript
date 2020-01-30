03617bc372121068c2b2244b9d01f74f7c59a258
Dominic <dominic.watts@hotmail.co.uk>
2020-01-30 22:26:54 +0000

    Lesson 5

---
+++ Block/Epsilon/Ko.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Xigen\Javascript\Block\Epsilon;
+
+/**
+ * Class Ko
+ * @package Xigen\Javascript\Block\Epsilon
+ */
+class Ko extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Constructor
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+}
